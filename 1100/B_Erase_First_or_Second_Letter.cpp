5cdd8063c9b27a68c6a30d2e879d662376cae632
Aadesh Phadake <aadeshphadake4@gmail.com>
2026-05-09 19:01:07 +0530

    1

---
+++ 1100/B_Erase_First_or_Second_Letter.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        set<char> st;
+        long long ans = 0;
+
+        for(char c : s){
+            st.insert(c);
+            ans += st.size();
+        }
+        cout<<ans<<endl;
+    }
+}
